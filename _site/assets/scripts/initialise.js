cb04879ea2fb73c6b0e9a66339de9cbf2b9f3ca6
John Puddephatt <john@jdp.org.uk>
2018-02-18 16:05:53 +0530

    restructure init scripts

---
+++ _site/assets/scripts/initialise.js
@@ -0,0 +1,35 @@
+var sliderImagesLoaded = 0;
+var sliderImages = document.querySelectorAll('#hero-image-slider img');
+var ldLoaded = false;
+
+
+document.addEventListener('DOMContentLoaded',function(){
+  document.documentElement.scrollTop = -100;
+  document.documentElement.classList.add('js-loading');
+  sliderImages.forEach(image => {
+    image.addEventListener('load',imageLoaded(image));
+  });
+});
+
+function imageLoaded(image){
+  sliderImagesLoaded++;
+  console.log('image loaded');
+  if(sliderImagesLoaded >= sliderImages.length && ldLoaded == false) {
+    // sliderImages.forEach(image => {
+    //   image.removeEventListener('load',imageLoaded());
+    // });
+    ldLoaded = true;
+    setTimeout(function(){
+      document.documentElement.classList.remove('js-loading');
+      simpleslider.getSlider({
+        container: document.getElementById('hero-image-slider'),
+        prop: 'opacity',
+        init: 0,
+        show: 1,
+        end: 0,
+        unit: '',
+        duration: 1
+      });
+    },4000);
+  }
+}
