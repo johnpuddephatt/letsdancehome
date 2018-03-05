f4e0a16e0cdf48a64128e81bcba4fd4cec8e4608
John Puddephatt <john@jdp.org.uk>
2018-03-05 23:50:42 +0000

    push

--- _site/assets/scripts/initialise.js
+++ _site/assets/scripts/initialise.js
@@ -1,9 +1,33 @@
-var sliderImagesLoaded = 0;
-var sliderImages = document.querySelectorAll('#hero-image-slider img');
-var ldLoaded = false;
 
 
 document.addEventListener('DOMContentLoaded',function(){
+  var sliderImagesLoaded = 0;
+  var sliderImages = document.querySelectorAll('#hero-image-slider img');
+  var ldLoaded = false;
+
+  function imageLoaded(image){
+    sliderImagesLoaded++;
+    console.log('image loaded');
+    if(sliderImagesLoaded >= sliderImages.length && ldLoaded == false) {
+      // sliderImages.forEach(image => {
+      //   image.removeEventListener('load',imageLoaded());
+      // });
+      ldLoaded = true;
+      setTimeout(function(){
+        document.documentElement.classList.remove('js-loading');
+        simpleslider.getSlider({
+          container: document.getElementById('hero-image-slider'),
+          prop: 'opacity',
+          init: 0,
+          show: 1,
+          end: 0,
+          unit: '',
+          duration: 1
+        });
+      },4000);
+    }
+  }
+
   document.documentElement.scrollTop = -100;
   document.documentElement.classList.add('js-loading');
   sliderImages.forEach(image => {
@@ -11,25 +35,3 @@ document.addEventListener('DOMContentLoaded',function(){
   });
 });
 
-function imageLoaded(image){
-  sliderImagesLoaded++;
-  console.log('image loaded');
-  if(sliderImagesLoaded >= sliderImages.length && ldLoaded == false) {
-    // sliderImages.forEach(image => {
-    //   image.removeEventListener('load',imageLoaded());
-    // });
-    ldLoaded = true;
-    setTimeout(function(){
-      document.documentElement.classList.remove('js-loading');
-      simpleslider.getSlider({
-        container: document.getElementById('hero-image-slider'),
-        prop: 'opacity',
-        init: 0,
-        show: 1,
-        end: 0,
-        unit: '',
-        duration: 1
-      });
-    },4000);
-  }
-}
