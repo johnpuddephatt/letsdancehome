00e6b5a49a8e903698f3ee0fa91007e35643d437
John Puddephatt <john@jdp.org.uk>
2019-01-20 22:56:44 +0000

    trying to keep style info in svg 4

--- _site/assets/scripts/initialise.js
+++ _site/assets/scripts/initialise.js
@@ -46,3 +46,50 @@ document.addEventListener('DOMContentLoaded',function(){
     image.addEventListener('load',imageLoaded(image));
   });
 });
+
+document.addEventListener('DOMContentLoaded',function(){
+
+  if ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
+
+    var aos = {
+      offset: 120,
+      delay: 0,
+      easing: "ease",
+      duration: 400,
+      disable: !1,
+      once: !1,
+      startEvent: "DOMContentLoaded"
+    };
+
+    document.body.classList.add('aos');
+    document.body.setAttribute("data-aos-easing", aos.easing),
+    document.body.setAttribute("data-aos-duration", aos.duration);
+    document.body.setAttribute("data-aos-delay", aos.delay);
+
+    var options = {
+      threshold: [0,.25,.5,.75],
+      rootMargin: '30px 30px'
+    };
+
+    var aos = document.querySelectorAll("[data-aos]");
+
+    var callback =  function(entries, observer) {
+      entries.forEach(entry => {
+        if(entry.intersectionRatio >= .5) {
+          entry.target.classList.add('aos-animate');
+        }
+        else if(entry.intersectionRatio == 0) {
+          entry.target.classList.remove('aos-animate');
+        }
+      });
+    }
+
+    var observer = new IntersectionObserver(callback, options);
+
+    aos.forEach(image => {
+      observer.observe(image);
+    });
+  }
+  
+});
+
